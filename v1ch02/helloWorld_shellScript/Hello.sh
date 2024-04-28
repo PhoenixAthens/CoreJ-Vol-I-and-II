22a2825ca9cb2de146f738a06f047bce7f751e3b
Phoenix <anmolkhanna2003@gmail.com>
2024-04-28 17:20:40 +0530

    Java class as a shell script

---
+++ v1ch02/helloWorld_shellScript/Hello.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/java --source 21 
+public class HelloSH{
+	public static void main(String... args){
+		System.out.println("I am a shell Script");
+		System.out.println("******HELLO********");
+	}
+}
+
